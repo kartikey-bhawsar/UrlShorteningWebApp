e1126bf237ad7f42b013bfc769ffd70402596874
Kartik <kartikbhavsar84@gmail.com>
2021-06-17 00:27:02 +0530

    First commit

---
+++ src/components/Globalstyle.js
@@ -0,0 +1,30 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    width: 100%;
+    margin: 0 auto;
+    font-size: 18px;
+    overflow-x: hidden;
+}
+*,body,h1,h2,h3,h4,a,li,input{
+    font-family: 'Poppins', sans-serif;
+}
+.container{
+    width: 80%;
+    margin: 0 auto;
+}
+  @media (max-width: 1100px){
+      img{
+          max-width: 100%;
+      }
+      .container{
+          width: 100%;
+      }
+  }
+`;
+
+export default GlobalStyle;
